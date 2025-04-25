e8672ff85d1856e6109ccd807192be99e70ab05c
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-04-25 19:16:29 +0900

    All-in-One WP Migrationのエクスポートデータから除外するディレクトリを設定の自動化

--- functions-lib/func-add-ai1wm-exclude.php
+++ functions-lib/func-add-ai1wm-exclude.php
@@ -5,12 +5,12 @@
 add_filter(
 	'ai1wm_exclude_themes_from_export',
 	function ($exclude_filters) {
-		$theme_name = 't_2025-01-11wp'; // テーマディレクトリを指定
-		$exclude_filters[] = $theme_name . '/.git';
-		$exclude_filters[] = $theme_name . '/.gitignore';
-		$exclude_filters[] = $theme_name . '/gulp';
-		$exclude_filters[] = $theme_name . '/src';
-		$exclude_filters[] = $theme_name . '/README.md';
+		$theme_dir = basename(get_theme_root() . '/' . get_stylesheet()); // 実際のテーマディレクトリ名を取得
+		$exclude_filters[] = $theme_dir . '/.git';
+		$exclude_filters[] = $theme_dir . '/.gitignore';
+		$exclude_filters[] = $theme_dir . '/gulp';
+		$exclude_filters[] = $theme_dir . '/src';
+		$exclude_filters[] = $theme_dir . '/README.md';
 
 		return $exclude_filters;
 	}
