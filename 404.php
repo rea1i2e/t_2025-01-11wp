98afd4bca5acd60210b51e047c29c6f02e525af2
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-05-24 06:57:28 +0900

    404、ベーススタイル修正

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
     <div class="p-404__inner l-inner">
       <h2 class ="p-404__title c-common-title">お探しのページは見つかりませんでした。</h2>
       <div class="p-404__btn">
-        <a class="c-btn" href="<?php echo esc_url(home_url($page)); ?>">TOPページへ</a>
+        <a class="c-btn" href="<?php page_path(); ?>">TOPページへ</a>
       </div>
     </div>
   </div>
