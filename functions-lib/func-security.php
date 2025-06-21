6fd311272ac07af4f73d326af7c0ae2454c5e3cc
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-06-21 18:33:46 +0900

    css, jsバージョン情報削除取り消し

--- functions-lib/func-security.php
+++ functions-lib/func-security.php
@@ -11,16 +11,6 @@
  */
 remove_action('wp_head', 'wp_generator');
 
-/**
- * CSS, JSのバージョン情報削除
- */
-function remove_cssjs_ver2( $src ) {
-  if ( strpos( $src, 'ver=' ) )
-      $src = remove_query_arg( 'ver', $src );
-  return $src;
-}
-add_filter( 'style_loader_src', 'remove_cssjs_ver2', 9999 );
-add_filter( 'script_loader_src', 'remove_cssjs_ver2', 9999 );
 
 /**
  * 投稿者一覧ページを自動で生成されないようにする
