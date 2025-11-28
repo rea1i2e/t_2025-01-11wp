d59f272b057334d7527e558a9cf306718e267d69
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-11-28 10:01:46 +0900

    functions.php自動読み込み

--- functions.php
+++ functions.php
@@ -1,54 +1,18 @@
 <?php
 
 /**
- * Functions
+ * functions-lib内のファイルを自動読み込み
+ * 個別に順序を決めて読み込みたい場合は、require_onceを使用
+ * （ループの中で再度読み込むが、require_onceなので、2回目は読み込まれない）
  */
-// 基本設定
-get_template_part('functions-lib/func-base');
 
-// セキュリティー対応
-get_template_part('functions-lib/func-security');
+// 個別に順序を決めて読み込み
+// require_once get_theme_file_path('functions-lib/func-xxx.php');
 
-// ページの自動生成を防ぐ
-get_template_part('functions-lib/func-no-auto-generate');
+// 一括読み込み（順序に依存しない場合のみ）
+foreach (glob(get_theme_file_path('functions-lib/*.php')) as $file) {
+  require_once $file;
+}
 
-// All-in-One WP Migrationによるエクスポート除外設定
-get_template_part('functions-lib/func-add-ai1wm-exclude');
 
-// ショートコードの設定
-get_template_part('functions-lib/func-shortcode');
-
-// URLのショートカット設定
-get_template_part('functions-lib/func-url');
-
-// スクリプト、スタイルシートの設定
-get_template_part('functions-lib/func-enqueue-assets');
-
-// デフォルト投稿タイプのラベル変更
-get_template_part('functions-lib/func-add-posttype-post');
-
-// カスタム投稿の設定
-get_template_part('functions-lib/func-add-posttype-works');
-
-// 投稿が指定した日数以内であるか判定（未設定の場合は7日）
-// ※未検証
-get_template_part('functions-lib/func-utility');
-
-// メインクエリのSP表示件数設定
-// ※未検証
-get_template_part('functions-lib/func-posts-edit');
-
-// メンテナンスモード
-// get_template_part('functions-lib/func-maintenance-mode');
-
-// YouTubeのoEmbedを修正（チャンネル外の関連動画を非表示）
-get_template_part('functions-lib/func-modify-youtube-oembed');
-
-// reCAPTCHAの表示制御
-get_template_part('functions-lib/func-recaptcha');
-
-// サムネイルの設定
-get_template_part('functions-lib/func-thumbnail');
-
-// ナビゲーションアイテムの設定
-get_template_part('functions-lib/func-nav-items');
+?>
